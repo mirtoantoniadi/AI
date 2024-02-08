12953ffffb282e302a2938ec006b572450f6ef6b
Myrto Antoniadi <149180108+mirtoantoniadi@users.noreply.github.com>
2024-02-08 10:29:59 +0200

    Exit if no arguments provided

--- Main.java
+++ Main.java
@@ -1,1 +1,9 @@
+import java.util.*;
 
+public class Main {
+    public static void main(String[] args) {
+        if(args.length < 1) { //if no definition is given it displays a message and terminates
+            System.out.println("No definitions were given.To run the program you have to put the size of the list and next to it the times.The execution is terminated.");
+            return;
+        }
+}
